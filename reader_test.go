9f83a8a47f8e39863a5c6d054cf4bb3c601c9647
castaneai <castaneai@castaneai.net>
2020-02-08 17:36:55 +0900

    fix test

--- reader_test.go
+++ reader_test.go
@@ -44,9 +44,22 @@ func TestPacketUnpacker_Transform_WithMultiPacket(t *testing.T) {
 	r := bytes.NewReader([]byte{0x05, 0x00, 0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x05, 0x00, 0x77, 0x6f, 0x72, 0x6c, 0x64})
 	br := &shortReader{r: r, size: 8}
 	pr := transform.NewReader(br, &PacketUnpacker{})
-	var b bytes.Buffer
-	if _, err := io.Copy(&b, pr); err != nil {
-		t.Fatal(err)
+
+	{
+		p1 := make([]byte, 1024)
+		n, err := pr.Read(p1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, "hello", string(p1[:n]))
 	}
-	assert.Equal(t, "helloworld", string(b.Bytes()))
+
+	{
+		p2 := make([]byte, 1024)
+		n, err := pr.Read(p2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, "world", string(p2[:n]))
+	}
 }
